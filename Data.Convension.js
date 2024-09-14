9b8bab0e8f133f27473e38c59a5b20a6c0c6facf
bhlezzdmplyy <felicitybhlezzgarbin@gmail.com>
2024-09-14 23:40:22 +0800

    Create Data.Convension.js

---
+++ Data.Convension.js
@@ -0,0 +1,14 @@
+// Convert a String to a Number
+let price = "200";
+
+// Method 1: Using parseInt()
+let priceParsedInt = parseInt(price);
+console.log("Using parseInt():", priceParsedInt); // Output: 200
+
+// Method 2: Using Number()
+let priceNumber = Number(price);
+console.log("Using Number():", priceNumber); // Output: 200
+
+// Method 3: Using the + operator
+let pricePlusOperator = +price;
+console.log("Using + operator:", pricePlusOperator); // Output: 200
