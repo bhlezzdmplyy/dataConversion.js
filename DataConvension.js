2c843b72a8bac29955a4d68e64c3c8a56234ae97
bhlezzdmplyy <felicitybhlezzgarbin@gmail.com>
2024-09-14 23:43:14 +0800

    Create DataConvension.js

---
+++ DataConvension.js
@@ -0,0 +1,10 @@
+// Convert a Number to a String
+let distance = 150;
+
+// Method 1: Using .toString()
+let distanceToString = distance.toString();
+console.log("Using toString():", distanceToString); // Output: "150"
+
+// Method 2: Using String()
+let distanceString = String(distance);
+console.log("Using String():", distanceString); // Output: "150"
